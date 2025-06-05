5f38797784860859b386c626cc8b51e526ec88a2
Chukwudi Ikem <chukwudi-ikem@super-rogatory.localdomain>
2025-06-04 21:42:12 -0700

    fetch frontend start

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true // clean output directory before emit
+    },
+    devServer: {
+        static: './dist',
+        port: 8080,
+        historyApiFallback: true, // support for react-router-dom
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html' // index bundle.js here
+        })
+    ]
+};
